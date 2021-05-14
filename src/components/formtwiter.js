65e6dd5fc1970e241fde75a6eef2163c15d7b40b
adanmc01 <dev.adanmc01@gmail.com>
2021-05-14 16:52:01 -0500

    firstCommit

--- src/components/formtwiter.js
+++ src/components/formtwiter.js
@@ -8,10 +8,11 @@ import List from './list'
 const cryptoRandomString = require("crypto-random-string");
 
 
-const FormTwiter = ({dataUser, setDataUser,setSenator,senator,mp, setMp}) => {
+const FormTwiter = ({dataUser, setDataUser, setSenator, senator, mp, setMp}) => {
     const [yourMP, setYourMP] = useState([])
     const [validated, setValidated] = useState(false);
     const [error, setError] = useState(false)
+    // const [tweetText, setTweetText] = useState({})
     const handleChange = e => {
         setDataUser({
             ...dataUser,
@@ -29,7 +30,7 @@ const FormTwiter = ({dataUser, setDataUser,setSenator,senator,mp, setMp}) => {
             e.stopPropagation();
         }
         setValidated(true);
-        if (firstName.trim() === '' || lastName.trim() === '' ||
+        if (//firstName.trim() === '' || lastName.trim() === '' || //
             zipCode.trim() === '' || email.trim() === '') {
             setError(true)
             return
@@ -44,6 +45,8 @@ const FormTwiter = ({dataUser, setDataUser,setSenator,senator,mp, setMp}) => {
         setSenator(dataPayload)
         setMp(getMp
         )
+        // const dataTweet = await axios.get('http://localhost:8080/taxPayers/tweet/1')
+        // setTweetText(dataTweet.data.data.text)
         if (yourMP) {
             //mostrar lista de MP´s
             console.log(yourMP)
@@ -55,85 +58,111 @@ const FormTwiter = ({dataUser, setDataUser,setSenator,senator,mp, setMp}) => {
 
 
     return (
-        <div className={'container'} style={{justifyContent: 'center', display: 'flex'}}>
-            <div style={{maxWidth: '700px', width: '100%'}}>
-                <h1>Tweet Your MP</h1>
-                {error ? <Alert variant={'danger'}>
-                    All fields are required!
-                </Alert> : null}
-                <Form noValidate validated={validated}>
-
-                    <Form.Group controlId="firstname">
-                        <Form.Control
-                            type="text"
-                            placeholder="Name"
-                            name="firstName"
-                            onChange={handleChange}
-                            required
-                            autoFocus={true}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="lastName">
-                        <Form.Control
-                            type="text"
-                            placeholder="LastName"
-                            name="lastName"
-                            onChange={handleChange}
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="cp">
-                        <Form.Control
-                            type="text"
-                            placeholder="Code Postal"
-                            name="zipCode"
-                            onChange={handleChange}
-                            required
-                            maxLength="4"
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="email">
-                        <Form.Control
-                            type="email"
-                            placeholder="Enter email"
-                            name="email"
-                            onChange={handleChange}
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group>
-                        <Button
-                            size={'lg'}
-                            onClick={click}
-                            className={'u-full-width'}
-                        >
-                            Save
-                        </Button>
-                    </Form.Group>
-                </Form>
-                <div>
-                    <h2>MP´s</h2>
-                    {mp.length > 0 && mp.filter(item => item.govt_type === 'Federal MPs').map((mps, index) => (
-                        <List
 
 
-                            mps={mps}
-                            key={index}
 
-                        />)
-                    )}
-                </div>
-                <div>
-                    <h2>Senators</h2>
-                    {senator.filter(item => item.govt_type === 'Federal Senators').map((mps, index) => (
-                            <div>
-                                <List
-                                    mps={mps}
-                                    key={index}
-                                />
-                            </div>
-                        )
-                    )}
+            <div className={'container'} style={{justifyContent: 'center', display: 'flex'}}>
+                <div style={{maxWidth: '700px', width: '100%'}}>
+                    <div style={{textAlign:'justify'}}>
+                        {/*debe de ser img esperando  logo*/}
+                        <h1 style={{textAlign:'center', padding:'55px'}} >Tweet Your MP</h1>
+                        <p style={{}}>By typing in a postcode, users can easily find their local MPs and Senators.
+                            From there,
+                            they can choose to tweet, call, or email them with just a few clicks
+                            The system is created
+                            to work seamlessly on both computers and smartphones, and is a great way to activate your
+                            followers and subscribers into political action.
+
+                        </p>
+                        <p style={{}}>
+                            This tool is currently built to contact Australian politicians, but can be adapted for any
+                            country and its general functions can easily be repurposed.
+                        </p>
+
+                    {error ? <Alert variant={'danger'}>
+                        All fields are required!
+                    </Alert> : null}
+                    <Form noValidate validated={validated}>
+
+                        {/*<Form.Group controlId="firstname">*/}
+                        {/*    <Form.Control*/}
+                        {/*        type="text"*/}
+                        {/*        placeholder="Name"*/}
+                        {/*        name="firstName"*/}
+                        {/*        onChange={handleChange}*/}
+                        {/*        required*/}
+                        {/*        autoFocus={true}*/}
+                        {/*    />*/}
+                        {/*</Form.Group>*/}
+                        {/*<Form.Group controlId="lastName">*/}
+                        {/*    <Form.Control*/}
+                        {/*        type="text"*/}
+                        {/*        placeholder="LastName"*/}
+                        {/*        name="lastName"*/}
+                        {/*        onChange={handleChange}*/}
+                        {/*        required*/}
+                        {/*    />*/}
+                        {/*</Form.Group>*/}
+                        <h3>Find you local MP here:</h3>
+                        <Form.Group controlId="email">
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                name="email"
+                                onChange={handleChange}
+                                required
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="cp">
+                            <Form.Control
+                                type="text"
+                                placeholder="Type your zipCode and press ENTER"
+                                name="zipCode"
+                                onChange={handleChange}
+                                required
+                                maxLength="4"
+                            />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Button
+                                size={'lg'}
+                                onClick={click}
+                                className={'u-full-width'}
+                            >
+                                Save
+                            </Button>
+                        </Form.Group>
+                    </Form>
+                    <div>
+                        <div>
+                            <p>NOTE: Choose only one Representative at a time.
+                                If you wish to contact more than one representative, or add further emails to the same
+                                Representative, you will have the option to repeat after sending each email.</p>
+                        </div>
+                        <h2>MP´s</h2>
+                        {mp.length > 0 && mp.filter(item => item.govt_type === 'Federal MPs').map((mps, index) => (
+                            <List
+                                // tweetText={tweetText}
+                                mps={mps}
+                                key={index}
+                            />)
+                        )}
+                    </div>
+                    <div>
+                        <h2>Senators</h2>
+                        {senator.filter(item => item.govt_type === 'Federal Senators').map((mps, index) => (
+                                <div>
+                                    <List
+                                        // tweetText={tweetText}
+                                        mps={mps}
+                                        key={index}
+                                    />
+                                </div>
+                            )
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/list.js
+++ src/components/list.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Button from "react-bootstrap/cjs/Button";
+import axios from 'axios'
 
 const List = ({mps}) => {
+
+    const tweetText = `.${mps.twitter}%2C+I%E2%80%99m+a+voter+in+your+electorate%2C+and+I+think+our+taxes+are+too+high%21+Australians+deserve+to+keep+more+of+their+own+money+and+I%E2%80%99m+asking+you+to+fight+in+Canberra+for+lower+taxes.+%0D%0A%0D%0ASent+from+%40AusTaxpayers%E2%80%99+Contact+Your+Politician+Platform.&original_referer=https://www.taxpayers.org.au/let-them-know-you-care`
     console.log(mps)
-    const tweetText = `I+am+a+voter+in+${mps.twitter}+district%2C+and+I+strongly+oppose+the+proposed+%23CashBan!+The+ban+is+a+slow-squeeze+on+Aussies+and+is+nothing+more+than+a+gift+to+big+banks+at+our+expense.+%23DontBanCash!&original_referer=https://www.taxpayers.org.au/dont-ban-cash`
     return (
         <div>
             <h3>{mps.name}</h3>
